b70fce0c00b219ebf9fd5a8c6ec322525b0c29aa
Reece Russell <reussell@protonmail.com>
2025-11-22 00:09:23 +0200

    Basic parsing pkgs str -> array; issues with sudo

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
 
 func main() {
-  fmt.Println("Hello World!")
+  // User input pkgs
+  fmt.Println("Please pkgs")
+  var userPkgs string
+
+  scanner := bufio.NewScanner(os.Stdin)
+  if scanner.Scan() {
+    userPkgs = scanner.Text()
+  }
+  userPkgArr := strings.Split(userPkgs, " ")
+  for _, pkg := range userPkgArr {
+    fmt.Println(pkg)
+  }
+
+  // Check for pkgs; need to check for pkg manager!
+  cmd := exec.Command("sudo", "-Ss", userPkgArr[0])
+  cmd.Stdin = os.Stdin
+  cmd.Stdout = os.Stdout
+  cmd.Stderr = os.Stderr
+  if err := cmd.Run(); err != nil {
+    fmt.Fprintf(os.Stderr, "failed")
+  }
+
 }
